d30afe7dffe7d699e11ac4f1ead107ded7107269
Nihal SIngh Chawla <74036257+Nihal-7@users.noreply.github.com>
2022-06-08 10:03:37 +0530

    Add files via upload

--- ProductRepository.java
+++ ProductRepository.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepository {
-List<Product> prod=new ArrayList<>();
+List<Product> pr=new ArrayList<>();
 public List<Product> getList(){
-Product product1=new Product(1,"powerbank",2000,"mobileAccesory");
-Product product2=new Product(2,"earphones",1100,"mobileAccesory");
-Product product3=new Product(3,"tshirts",30,"clothing");
-Product product4=new Product(4,"shirts",450,"clothing");
-Product product5=new Product(5,"hats",1450,"accesory");
-Product product6=new Product(6,"purse",1780,"accesory");
-prod.add(product1);
-prod.add(product2);
-prod.add(product3);
-prod.add(product4);
-prod.add(product5);
-prod.add(product6);
-return prod;
+Product p1=new Product(1,"bag",1200,"stationary");
+Product p2=new Product(2,"pen",12,"stationary");
+Product p3=new Product(3,"nachos",30,"food");
+Product p4=new Product(4,"cake",450,"bakery");
+Product p5=new Product(5,"jeans",1450,"clothing");
+Product p6=new Product(6,"shirt",1780,"clothing");
+pr.add(p1);
+pr.add(p2);
+pr.add(p3);
+pr.add(p4);
+pr.add(p5);
+pr.add(p6);
+return pr;
 }
 
 
